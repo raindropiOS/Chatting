5dfe66faf60626913f6c3bba058df52afa15c084
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2022-12-10 17:35:46 +0900

    add id, pw view

--- Chatting/View/LoginView.swift
+++ Chatting/View/LoginView.swift
@@ -10,15 +10,16 @@ import SwiftUI
 struct LoginView: View {
     @State var isSingnInPresented: Bool = false
     @ObservedObject var carStore: CarStore
+    @State private var inputID: String = ""
+    @State private var inputPassword: String = ""
     
     var body: some View {
         VStack {
-            
+            InputIDPW(inputID: $inputID, inputPassword: $inputPassword)
             Button("let's Sign-in") {
                 isSingnInPresented.toggle()
             }
             .foregroundColor(.blue)
-        
         }
         .sheet(isPresented: $isSingnInPresented) {
             SignIn(carStore: carStore, isSingnInPresented: $isSingnInPresented)
@@ -29,5 +30,26 @@ struct LoginView: View {
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView(carStore: CarStore())
+    }
+}
+
+struct InputIDPW: View {
+    @Binding var inputID: String
+    @Binding var inputPassword: String
+    var body: some View {
+        VStack {
+            HStack {
+                Text("ID")
+                TextField("", text: $inputID)
+                    .border(.black)
+            }
+            
+            HStack {
+                Text("PW")
+                TextField("", text: $inputPassword)
+                    .border(.black)
+            }
+        }
+        .frame(width: 200)
     }
 }
